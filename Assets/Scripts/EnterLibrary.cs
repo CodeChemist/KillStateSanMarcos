9d9752f481b4e9abb2affe1848345f466ce76d4b
CodeChemist <finleycreative@gmail.com>
2015-03-22 21:24:03 -0700

    Unity3D Project Files
    Files for game project, and some Maya models

---
+++ Assets/Scripts/EnterLibrary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterLibrary : MonoBehaviour {
+
+	void OnTriggerEnter(){
+
+		
+		// load next scene
+		Application.LoadLevel (1);
+	}
+}
---
+++ Assets/Scripts/ExitLibrary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitLibrary : MonoBehaviour {
+
+	void OnTriggerEnter(){
+
+		
+		// load next scene
+		Application.LoadLevel (0);
+	}
+}
