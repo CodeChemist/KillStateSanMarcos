31f5b99f30f2690de3674bab90c44825aadb8a8b
c-perdue <perdu006@csusm.edu>
2015-05-03 13:34:24 -0700

    Zombie Attack Health UI merging
    Cleaned up some files, erased extra copies, began merging code between
    PlayerHealth and Motor.

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -1,4 +1,7 @@
-﻿/*
+﻿/*                  //depricated due to namespace error: Already contains definition for EnemyAttack 
+ *                      (Chase --- 5/2/15)
+ * 
+
 	Chris Finley
 	CS485 Assignment 1
 	2D Platformer
@@ -7,7 +10,7 @@
 	This script controls the collider to determin when
 	the player is close enough for the enemy to play
 	it's attack sound and animations
-*/
+
 
 using UnityEngine;
 using System.Collections;
@@ -28,3 +31,5 @@ public class EnemyAttack : MonoBehaviour {
 		}
 	}
 }
+
+*/
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -10,95 +10,143 @@
  * Class:       CS485 - Game Programming
  * 
  * Changelog:
- *      + 4/26/15 ------ Initial version. - Chase Perdue
+ *      + 4/26/15 --------- Initial version. - Chase Perdue
+ *      + 5/2/15  --------- Re-made via VS2013's auto-create missing reference feature. 
+ *                          Filename is missing (chase) for now and it is confusing 
+ *                          and frustrating. Will change on restart.
+ *      + 5/3/15  --------- Changed filename to resolve conflict. Added comments as needed.
  */
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerHealth : MonoBehaviour
+namespace Assets.Scripts
 {
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-    //public AudioClip deathClip;           //I don't have a clip for that.
-    public float flashSpeed = 5f;           //used in the damageImage control
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     //red, mostly transparent full screen image
-
-
-    //Animator anim;      
-    //AudioSource playerAudio;
-
-    //PlayerMovement playerMovement;        //reference to previously made script in the tutorial. 
-                                            //not used in this project. does Harrison have something already?
-
-    //PlayerShooting playerShooting;        //this line was commented out when I get here...
-    bool isDead;
-    bool damaged;
-
-
-    void Awake ()
+    /// <summary>
+    /// Class PlayerHealth should govern all aspects pertaining to the player's health as 
+    /// they play the game. Health is a value that can be lost and gained depending on 
+    /// external influences such as health pickups and enemy attacks.
+    /// </summary>
+    public class PlayerHealth : MonoBehaviour
     {
-        //anim = GetComponent <Animator> ();                    
-        //playerAudio = GetComponent <AudioSource> ();
-        //playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();        //this line was commented out when I get here...
-        currentHealth = startingHealth;
-    }
+        public int startingHealth = 100;
+        public int currentHealth;
+        public Slider healthSlider;
+        public Image damageImage;
 
+        //I don't have a clip for this.
+        //public AudioClip deathClip; 
 
-    void Update ()
-    {
-        if(damaged)
+        //used in the damageImage control
+        public float flashSpeed = 5f;           
+
+        //red, mostly transparent full screen image
+        public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     
+
+        //Harrison already has this.
+        //Animator anim;        
+
+        //Don't have this yet.
+        //AudioSource playerAudio;
+
+        //reference to previously made script in the tutorial. 
+        //PlayerMovement playerMovement;        
+        
+        //this line was commented out when I get here...
+        //PlayerShooting playerShooting;        
+        
+        bool isDead;
+        bool damaged;
+
+        /// <summary>
+        /// Awake initializes health variable upon instantiation.
+        /// </summary>
+        void Awake()
         {
-            damageImage.color = flashColour;
+            //These three lines don't have refrences...yet.
+            //anim = GetComponent <Animator> ();                    
+            //playerAudio = GetComponent <AudioSource> ();
+            //playerMovement = GetComponent <PlayerMovement> ();
+
+            //this line was commented out when I get here...
+            //playerShooting = GetComponentInChildren <PlayerShooting> ();        
+            
+            currentHealth = startingHealth;
         }
-        else
+
+        /// <summary>
+        /// As a player recieves damage, a full screen red image will flash. Not taking damege, 
+        /// color fades away.
+        /// </summary>
+        void Update()
         {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            if (damaged)
+            {
+                damageImage.color = flashColour;
+            }
+            else
+            {
+                damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            }
+            damaged = false;
         }
-        damaged = false;
-    }
 
-
-    public void TakeDamage (int amount)
-    {
-        damaged = true;
-
-        currentHealth -= amount;
-
-        healthSlider.value = currentHealth;
-
-        //playerAudio.Play ();
-
-        if(currentHealth <= 0 && !isDead)
+        /// <summary>
+        /// TakeDamage takes health away from the player according to sucessful enemy attacks.
+        /// Player health decreases as does the inset bar on the health slider. If the player's 
+        /// health goes too low, they die.
+        /// Called by a script outside of this file.
+        /// </summary>
+        /// <param name="amount">The value of damage an enemy has inflicted in a single strike.</param>
+        public void TakeDamage(int amount)
         {
-            Death ();
+            damaged = true;
+
+            currentHealth -= amount;
+
+            healthSlider.value = currentHealth;
+
+            //playerAudio.Play ();
+
+            if (currentHealth <= 0 && !isDead)
+            {
+                Death();
+            }
         }
-    }
+
+        /// <summary>
+        /// Handles player dealth event. When player health reaches 0 or lower (from TakeDamage method),
+        /// the isDead plag is set to true and any game over messages will then appear.
+        /// 
+        /// TODO:
+        ///     + player death animations, sounds etc.
+        ///     + player death menu/screen
+        ///     + How should movement be disabled?
+        /// </summary>
+        void Death()
+        {
+            isDead = true;
+
+            //playerShooting.DisableEffects ();        //this line was commented out when I get here...
+
+            // invalid reference chase, needs fixing ~chris
+            //anim.SetTrigger ("Die");
+
+            //playerAudio.clip = deathClip;
+            //playerAudio.Play ();
+
+            //playerMovement.enabled = false;
+            //playerShooting.enabled = false;        //this line was commented out when I get here...
+        }
 
 
-    void Death ()
-    {
-        isDead = true;
-
-        //playerShooting.DisableEffects ();        //this line was commented out when I get here...
-
-		// invalid reference chase, needs fixing ~chris
-        //anim.SetTrigger ("Die");
-
-        //playerAudio.clip = deathClip;
-        //playerAudio.Play ();
-
-        //playerMovement.enabled = false;
-        //playerShooting.enabled = false;        //this line was commented out when I get here...
-    }
-
-
-    public void RestartLevel ()
-    {
-        Application.LoadLevel (Application.loadedLevel);
+        public void RestartLevel()
+        {
+            Application.LoadLevel(Application.loadedLevel);    //How is this even called?
+        }
     }
 }
