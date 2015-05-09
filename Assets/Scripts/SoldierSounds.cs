1c6139d8cca6959c9cb4c9a160a79830e73c98ce
c-perdue <perdu006@csusm.edu>
2015-05-08 22:46:12 -0700

    Sound Script framework added

---
+++ Assets/Scripts/SoldierSounds.cs
@@ -0,0 +1,41 @@
+/*
+ * 
+ * ------------------------------- SoldierSounds.cs -------------------------------
+ * 
+ * Purpose:     To manage the sounds of soldiers.
+ * 
+ * Created By:  Chase Perdue
+ * Date:        5/8/15
+ * Class:       CS485 - Game Programming
+ * 
+ * Changelog:
+ *      + 5/8/15 ------ Initial version. - Chase Perdue
+ */
+using UnityEngine;
+using System.Collections;
+
+public class SoldierSounds : MonoBehaviour
+{
+	public AudioClip radioChatterShort;
+	public AudioClip radioChatterLong;
+	public AudioClip enemySighted;	
+	public AudioClip warCry1;
+	public AudioClip warCry2;
+	public AudioClip warCry3;
+	
+	private AudioSource source;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
